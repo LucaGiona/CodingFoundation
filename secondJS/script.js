9ab2d2f6a9bb6e8d75e882ec29c6a0c3efc71362
LucaGiona <luca@senzproduction.com>
2023-10-18 15:27:56 +0200

    Change File Names

---
+++ secondJS/script.js
@@ -0,0 +1,16 @@
+console.log("Hallo Welt!");
+console.log("Again and again!");
+"Hello World".toUpperCase();//console.log
+console.log("Hello World".toUpperCase());
+console.log(1);
+console.log(1.3);
+console.log(1.3.toFixed());//ist eine Methode erster Punkt Komma bei zahl zweiter  PUnkt MAO
+console.log(1,3);
+
+console.log(1);
+
+console.log("Hallo    Welt!".length);
+console.log("Hallo \"Welt!\""); 
+//doule quotes - maskieren
+console.log("Hallo Welt!"[4]);
+console.log("Hallo Welt!".substring(4,9));
